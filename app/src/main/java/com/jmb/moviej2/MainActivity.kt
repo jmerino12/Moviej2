1d4122d4edf20a07be58f3ef8a0a58882645edaa
Jonathan Meriño Bolivar <jmerino1204@gmail.com>
2021-08-28 12:57:44 -0500

    Mostrar titulos de los resultados en el adapter

--- app/src/main/java/com/jmb/moviej2/MainActivity.kt
+++ app/src/main/java/com/jmb/moviej2/MainActivity.kt
@@ -2,19 +2,26 @@ package com.jmb.moviej2
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.jmb.moviej2.databinding.ActivityMainBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val adapter = MoviesAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.recycler.adapter = adapter
 
         GlobalScope.launch(Dispatchers.Main) {
             val movies = MovieDB.service.listPopularMoviesAsync(getString(R.string.api_key))
-            toast(movies.results.size.toString())
+            adapter.movies = movies.results
         }
     }
 }
