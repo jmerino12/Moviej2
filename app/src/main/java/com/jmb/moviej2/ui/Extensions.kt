74a19089c21839a5f9c1a9d238da6bc73388c553
Jonathan Meriño Bolivar <jmerino1204@gmail.com>
2021-08-28 16:22:33 -0500

    Quitar la extensión de Toast sin usar

--- app/src/main/java/com/jmb/moviej2/ui/Extensions.kt
+++ app/src/main/java/com/jmb/moviej2/ui/Extensions.kt
@@ -1,20 +1,14 @@
 package com.jmb.moviej2.ui
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
-
-fun Context.toast(message: String) {
-    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-}
 
 fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = true): View =
     LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
